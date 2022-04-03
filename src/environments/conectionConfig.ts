cb1f5cd59cd71b689d7e1e97ed0d78fc419957d6
Gabriel Franco <francogabrielpy@outlook.com>
2022-04-03 14:13:55 -0400

    nada raro

--- src/environments/conectionConfig.ts
+++ src/environments/conectionConfig.ts
@@ -1,5 +1,5 @@
 export const ipAddress = 'localhost'
-export const port = 8082
+export const port = 8081
 // export const ipAddress = '172.25.0.3' // katuete 2
 // export const ipAddress = '172.25.0.1'
 
