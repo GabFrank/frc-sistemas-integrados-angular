72ad36dd5c48b9ff4a6122f9dfbadbf96415bc97
Diego <diegoamarilla478@gmail.com>
2025-11-29 10:59:35 -0300

    consistencia de datos

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -9,12 +9,12 @@ export const environment = {
   production: false,
   usuario : 1,
   sucursalId: 1,
-  firebaseConfig : {
-    apiKey: "AIzaSyDAPq38fcPq-8qtSbQ_YyTc0Vc0pqlqWV4",
-    authDomain: "franco-system.firebaseapp.com",
-    projectId: "franco-system",
-    storageBucket: "franco-system.appspot.com",
-    messagingSenderId: "389460380308",
-    appId: "1:389460380308:web:53701896405855d9f64281"
-  }
+ firebaseConfig: {
+  apiKey: "AIzaSyDAPq38fcPq-8qtSbQ_YyTc0Vc0pqlqWV4",
+  authDomain: "bodega-franco-frc.firebaseapp.com",
+  projectId: "bodega-franco-frc",
+  storageBucket: "bodega-franco-frc.appspot.com",
+  messagingSenderId: "170136643206",
+  appId: "1:170136643206:web:b02e8a1da223c98da5d307"
+},
 };
