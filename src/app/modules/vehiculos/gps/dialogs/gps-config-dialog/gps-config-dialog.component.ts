09ba7c2ab2bedebb1c9ec2635d1e02e097ad7a62
Diego <diegoamarilla478@gmail.com>
2026-03-15 15:17:36 -0300

    mejoras en gps

--- src/app/modules/vehiculos/gps/dialogs/gps-config-dialog/gps-config-dialog.component.ts
+++ src/app/modules/vehiculos/gps/dialogs/gps-config-dialog/gps-config-dialog.component.ts
@@ -1,10 +1,23 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnInit, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Gps } from '../../models/gps.model';
 import { GpsService } from '../../service/gps.service';
 import { take, timeout } from 'rxjs/operators';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+interface LoadingState {
+  motor: boolean;
+  sleep: boolean;
+  interval: boolean;
+  apn: boolean;
+  alertaVelocidad: boolean;
+  alertaVibracion: boolean;
+  alertaBateriaBaja: boolean;
+  alertaAcc: boolean;
+}
+
+@UntilDestroy()
 @Component({
   selector: 'app-gps-config-dialog',
   templateUrl: './gps-config-dialog.component.html',
@@ -12,13 +25,16 @@ import { take, timeout } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GpsConfigDialogComponent implements OnInit {
-  private fb = inject(FormBuilder);
-  private gpsService = inject(GpsService);
-  private cdr = inject(ChangeDetectorRef);
+  private readonly fb = inject(FormBuilder);
+  private readonly gpsService = inject(GpsService);
+  private readonly cdr = inject(ChangeDetectorRef);
+  private readonly dialogRef = inject(MatDialogRef<GpsConfigDialogComponent>);
+  private readonly data = inject<Gps>(MAT_DIALOG_DATA);
 
   configForm: FormGroup;
-  gps: Gps;
-  loading: { [key: string]: boolean } = {
+  gps: Gps = this.data;
+  
+  loading: LoadingState = {
     motor: false,
     sleep: false,
     interval: false,
@@ -29,13 +45,6 @@ export class GpsConfigDialogComponent implements OnInit {
     alertaAcc: false
   };
 
-  constructor(
-    public dialogRef: MatDialogRef<GpsConfigDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Gps
-  ) {
-    this.gps = data;
-  }
-
   ngOnInit(): void {
     this.initForm();
   }
@@ -45,154 +54,153 @@ export class GpsConfigDialogComponent implements OnInit {
       motorEstado: [!this.gps.motorBloqueado],
       sleepMode: [this.gps.modoSueno || false],
       reportInterval: [this.gps.intervaloReporte || 30],
-
       velocidadLimite: [this.gps.velocidadLimite ?? 100],
       alertaVelocidad: [this.gps.alertaVelocidad ?? true],
       alertaVibracion: [this.gps.alertaVibracion ?? false],
       alertaBateriaBaja: [this.gps.alertaBateriaBaja ?? true],
       alertaAcc: [this.gps.alertaAcc ?? true],
-
-      // APN
       apnName: ['internet']
     });
   }
 
-  async onUpdateMotor(): Promise<void> {
+  getControlValue(controlName: string): any {
+    return this.configForm.get(controlName)?.value;
+  }
+
+  getMotorLabel(): string {
+    return this.getControlValue('motorEstado') ? 'MOTOR ENCENDIDO' : 'MOTOR BLOQUEADO';
+  }
+
+  onUpdateMotor(): void {
     this.loading.motor = true;
     this.cdr.markForCheck();
-    const motorTipo = this.configForm.get('motorEstado')?.value ? 'MOTOR_ON' : 'MOTOR_OFF';
-    return new Promise((resolve) => {
-      this.gpsService.onEnviarComando(this.gps.id, motorTipo)
-        .pipe(
-          take(1),
-          timeout(10000)
-        )
-        .subscribe({
-          next: () => { 
-            this.loading.motor = false; 
-            this.gps.motorBloqueado = !this.configForm.get('motorEstado')?.value;
-            this.cdr.markForCheck(); 
-            resolve(); 
-          },
-          error: () => { 
-            this.loading.motor = false; 
-            this.cdr.markForCheck(); 
-            resolve(); 
-          }
-        });
-    });
+    const motorTipo = this.getControlValue('motorEstado') ? 'MOTOR_ON' : 'MOTOR_OFF';
+    
+    this.gpsService.onEnviarComando(this.gps.id, motorTipo)
+      .pipe(
+        take(1),
+        timeout(10000),
+        untilDestroyed(this)
+      )
+      .subscribe({
+        next: () => { 
+          this.loading.motor = false; 
+          this.gps.motorBloqueado = !this.getControlValue('motorEstado');
+          this.cdr.markForCheck(); 
+        },
+        error: () => { 
+          this.loading.motor = false; 
+          this.cdr.markForCheck(); 
+        }
+      });
   }
 
-  async onUpdateSleep(): Promise<void> {
+  onUpdateSleep(): void {
     this.loading.sleep = true;
     this.cdr.markForCheck();
-    const sleepTipo = this.configForm.get('sleepMode')?.value ? 'SLEEP_ON' : 'SLEEP_OFF';
-    return new Promise((resolve) => {
-      this.gpsService.onEnviarComando(this.gps.id, sleepTipo)
-        .pipe(
-          take(1),
-          timeout(10000)
-        )
-        .subscribe({
-          next: () => { 
-            this.loading.sleep = false; 
-            this.gps.modoSueno = this.configForm.get('sleepMode')?.value;
-            this.cdr.markForCheck(); 
-            resolve(); 
-          },
-          error: () => { 
-            this.loading.sleep = false; 
-            this.cdr.markForCheck(); 
-            resolve(); 
-          }
-        });
-    });
+    const sleepTipo = this.getControlValue('sleepMode') ? 'SLEEP_ON' : 'SLEEP_OFF';
+    
+    this.gpsService.onEnviarComando(this.gps.id, sleepTipo)
+      .pipe(
+        take(1),
+        timeout(10000),
+        untilDestroyed(this)
+      )
+      .subscribe({
+        next: () => { 
+          this.loading.sleep = false; 
+          this.gps.modoSueno = this.getControlValue('sleepMode');
+          this.cdr.markForCheck(); 
+        },
+        error: () => { 
+          this.loading.sleep = false; 
+          this.cdr.markForCheck(); 
+        }
+      });
   }
 
-  async onUpdateInterval(): Promise<void> {
+  onUpdateInterval(): void {
     this.loading.interval = true;
     this.cdr.markForCheck();
-    const interval = this.configForm.get('reportInterval')?.value;
-    return new Promise((resolve) => {
-      this.gpsService.onEnviarComando(this.gps.id, 'INTERVALO', interval.toString())
-        .pipe(
-          take(1),
-          timeout(10000)
-        )
-        .subscribe({
-          next: () => { 
-            this.loading.interval = false; 
-            this.gps.intervaloReporte = this.configForm.get('reportInterval')?.value;
-            this.cdr.markForCheck(); 
-            resolve(); 
-          },
-          error: () => { 
-            this.loading.interval = false; 
-            this.cdr.markForCheck(); 
-            resolve(); 
-          }
-        });
-    });
+    const intervalValue = this.getControlValue('reportInterval');
+    
+    this.gpsService.onEnviarComando(this.gps.id, 'INTERVALO', intervalValue.toString())
+      .pipe(
+        take(1),
+        timeout(10000),
+        untilDestroyed(this)
+      )
+      .subscribe({
+        next: () => { 
+          this.loading.interval = false; 
+          this.gps.intervaloReporte = intervalValue;
+          this.cdr.markForCheck(); 
+        },
+        error: () => { 
+          this.loading.interval = false; 
+          this.cdr.markForCheck(); 
+        }
+      });
   }
 
-  async onUpdateApn(): Promise<void> {
+  onUpdateApn(): void {
     this.loading.apn = true;
     this.cdr.markForCheck();
-    const apn = this.configForm.get('apnName')?.value;
+    const apn = this.getControlValue('apnName');
     const valor = [apn, '', ''].join(',');
-    return new Promise((resolve) => {
-      this.gpsService.onEnviarComando(this.gps.id, 'APN', valor)
-        .pipe(
-          take(1),
-          timeout(10000)
-        )
-        .subscribe({
-          next: () => { this.loading.apn = false; this.cdr.markForCheck(); resolve(); },
-          error: () => { this.loading.apn = false; this.cdr.markForCheck(); resolve(); }
-        });
-    });
+    
+    this.gpsService.onEnviarComando(this.gps.id, 'APN', valor)
+      .pipe(
+        take(1),
+        timeout(10000),
+        untilDestroyed(this)
+      )
+      .subscribe({
+        next: () => { 
+          this.loading.apn = false; 
+          this.cdr.markForCheck(); 
+        },
+        error: () => { 
+          this.loading.apn = false; 
+          this.cdr.markForCheck(); 
+        }
+      });
   }
 
-  /**
-   * Guarda la configuración de una alerta push en la BD.
-   */
-  async onGuardarAlerta(tipo: string): Promise<void> {
+  onGuardarAlerta(tipo: keyof LoadingState): void {
     this.loading[tipo] = true;
     this.cdr.markForCheck();
-    const form = this.configForm;
-    return new Promise((resolve) => {
-      this.gpsService.onGuardarConfigAlertas(
-        this.gps.id,
-        form.get('alertaVelocidad')?.value,
-        form.get('velocidadLimite')?.value,
-        form.get('alertaVibracion')?.value,
-        form.get('alertaBateriaBaja')?.value,
-        form.get('alertaAcc')?.value
+    
+    this.gpsService.onGuardarConfigAlertas(
+      this.gps.id,
+      this.getControlValue('alertaVelocidad'),
+      this.getControlValue('velocidadLimite'),
+      this.getControlValue('alertaVibracion'),
+      this.getControlValue('alertaBateriaBaja'),
+      this.getControlValue('alertaAcc')
+    )
+      .pipe(
+        take(1),
+        timeout(10000),
+        untilDestroyed(this)
       )
-        .pipe(
-          take(1),
-          timeout(10000)
-        )
-        .subscribe({
-          next: (gpsActualizado) => {
-            this.loading[tipo] = false;
-            if (gpsActualizado) {
-              // Actualizar el GPS local con los datos persistidos
-              this.gps = { ...this.gps, ...gpsActualizado };
-            }
-            this.cdr.markForCheck();
-            resolve();
-          },
-          error: () => {
-            this.loading[tipo] = false;
-            this.cdr.markForCheck();
-            resolve();
+      .subscribe({
+        next: (gpsActualizado) => {
+          this.loading[tipo] = false;
+          if (gpsActualizado) {
+            this.gps = { ...this.gps, ...gpsActualizado };
           }
-        });
-    });
+          this.cdr.markForCheck();
+        },
+        error: () => {
+          this.loading[tipo] = false;
+          this.cdr.markForCheck();
+        }
+      });
   }
 
   onCancel(): void {
     this.dialogRef.close();
   }
 }
+
--- src/app/modules/vehiculos/gps/dialogs/gps-form/gps.component.ts
+++ src/app/modules/vehiculos/gps/dialogs/gps-form/gps.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnInit, Optional, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
@@ -17,21 +17,50 @@ import { GpsInput } from '../../models/gps-input.model';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GpsComponent implements OnInit {
-    private fb = inject(FormBuilder);
-    private gpsService = inject(GpsService);
-    private matDialog = inject(MatDialog);
-    private cdr = inject(ChangeDetectorRef);
-    private vehiculoSearchPageGQL = inject(VehiculoSearchPageGQL);
-
-    constructor(
-        @Optional() public dialogRef: MatDialogRef<GpsComponent>,
-        @Optional() @Inject(MAT_DIALOG_DATA) public data: Gps
-    ) { }
+    private readonly fb = inject(FormBuilder);
+    private readonly gpsService = inject(GpsService);
+    private readonly matDialog = inject(MatDialog);
+    private readonly cdr = inject(ChangeDetectorRef);
+    private readonly vehiculoSearchPageGQL = inject(VehiculoSearchPageGQL);
+    public readonly dialogRef = inject(MatDialogRef<GpsComponent>);
+    public readonly data = inject<Gps>(MAT_DIALOG_DATA);
 
     form: FormGroup;
-    gps: Gps;
+    gps: Gps = this.data;
     vehiculoSelected: Vehiculo | null = null;
     vehiculoDescripcion: string = 'SELECCIONE UN VEHICULO';
+
+    ngOnInit(): void {
+        this.inicializarFormulario();
+
+        if (this.gps?.id) {
+            this.cargarDatos();
+        }
+    }
+
+    private inicializarFormulario(): void {
+        this.form = this.fb.group({
+            id: [this.gps?.id || null],
+            imei: [this.gps?.imei || null, [Validators.required]],
+            modeloTracker: [this.gps?.modeloTracker || null, [Validators.required]],
+            simNumero: [this.gps?.simNumero || null],
+            vehiculoId: [this.gps?.vehiculo?.id || null],
+            activo: [this.gps?.id ? this.gps.activo : true]
+        });
+
+        if (this.gps?.vehiculo) {
+            this.vehiculoSelected = this.gps.vehiculo;
+            this.actualizarVehiculoDescripcion();
+        }
+    }
+
+    private cargarDatos(): void {
+        if (this.gps.vehiculo) {
+            this.vehiculoSelected = this.gps.vehiculo;
+            this.actualizarVehiculoDescripcion();
+        }
+        this.cdr.markForCheck();
+    }
 
     private actualizarVehiculoDescripcion(): void {
         if (this.vehiculoSelected) {
@@ -41,43 +70,12 @@ export class GpsComponent implements OnInit {
         }
     }
 
-    ngOnInit(): void {
-        this.gps = this.data;
-        this.inicializarFormulario();
-
-        if (this.gps?.id) {
-            this.cargarDatos();
-        } else {
-            this.form.patchValue({ activo: true });
-        }
+    getControlValue(controlName: string): any {
+        return this.form.get(controlName)?.value;
     }
 
-    private inicializarFormulario(): void {
-        this.form = this.fb.group({
-            id: [null],
-            imei: [null, [Validators.required]],
-            modeloTracker: [null, [Validators.required]],
-            simNumero: [null],
-            vehiculoId: [null],
-            activo: [true]
-        });
-    }
-
-    private cargarDatos(): void {
-        this.form.patchValue({
-            id: this.gps.id,
-            imei: this.gps.imei,
-            modeloTracker: this.gps.modeloTracker,
-            simNumero: this.gps.simNumero,
-            vehiculoId: this.gps.vehiculo?.id,
-            activo: this.gps.activo
-        });
-
-        if (this.gps.vehiculo) {
-            this.vehiculoSelected = this.gps.vehiculo;
-            this.actualizarVehiculoDescripcion();
-        }
-        this.cdr.markForCheck();
+    hasError(controlName: string, errorName: string): boolean {
+        return this.form.get(controlName)?.hasError(errorName) || false;
     }
 
     onBuscarVehiculo(): void {
@@ -88,7 +86,7 @@ export class GpsComponent implements OnInit {
             { id: 'modelo.descripcion', nombre: 'Modelo' }
         ];
 
-        const data: SearchListtDialogData = {
+        const searchData: SearchListtDialogData = {
             query: this.vehiculoSearchPageGQL,
             tableData,
             titulo: 'Buscar Vehículo',
@@ -100,7 +98,7 @@ export class GpsComponent implements OnInit {
         };
 
         this.matDialog.open(SearchListDialogComponent, {
-            data,
+            data: searchData,
             width: '70%',
             height: '80%'
         }).afterClosed().pipe(untilDestroyed(this)).subscribe((res: Vehiculo) => {
@@ -144,3 +142,4 @@ export class GpsComponent implements OnInit {
         this.dialogRef.close();
     }
 }
+
